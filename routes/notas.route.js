f47186e0559c92d32898d2831a441a12516cbe35
Nicolas Salgado <2000nsalgado@gmail.com>
2025-06-14 13:56:13 -0300

    document: inline documentação explicando as rotas

--- routes/notas.route.js
+++ routes/notas.route.js
@@ -2,10 +2,13 @@ import express from 'express';
 import { getNotas, createNota, updateNota, deleteNota } from '../controllers/nota.controller.js';
 
 const notasRouter = express.Router();
-
+// api/notas
 notasRouter.get('/', getNotas);
+// api/notas
 notasRouter.post('/', createNota);
+// api/notas/:id
 notasRouter.put('/:id', updateNota);
+// api/notas/:id
 notasRouter.delete('/:id', deleteNota);
 
 export default notasRouter;
